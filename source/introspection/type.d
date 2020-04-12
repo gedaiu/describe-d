540a7b3a07040dda0fde5c423061d5c1e5c5001f
Szabo Bogdan <szabobogdan@yahoo.com>
2020-04-12 15:49:08 +0200

    detect interface types

--- source/introspection/type.d
+++ source/introspection/type.d
@@ -25,6 +25,9 @@ struct Type {
   bool isClass;
 
   ///
+  bool isInterface;
+
+  ///
   bool isUnion;
 
   ///
@@ -81,6 +84,10 @@ Type describeType(T)() {
 
   static if(is(T == union)) {
     type.isUnion = true;
+  }
+
+  static if(is(T == interface)) {
+    type.isInterface = true;
   }
 
   static if(isArray!T) {
@@ -356,6 +363,27 @@ unittest {
   result.isStruct.should.equal(false);
   result.isClass.should.equal(false);
   result.isUnion.should.equal(true);
+  result.isBasicType.should.equal(false);
+  result.isBuiltinType.should.equal(false);
+  result.isConst.should.equal(false);
+  result.isInout.should.equal(false);
+  result.isImmutable.should.equal(false);
+  result.isShared.should.equal(false);
+}
+
+/// It should describe an interface
+unittest {
+  interface Test {}
+
+  auto result = describeType!Test;
+
+  result.name.should.equal("Test");
+  result.unqualName.should.equal("Test");
+
+  result.isStruct.should.equal(false);
+  result.isClass.should.equal(false);
+  result.isUnion.should.equal(false);
+  result.isInterface.should.equal(true);
   result.isBasicType.should.equal(false);
   result.isBuiltinType.should.equal(false);
   result.isConst.should.equal(false);
